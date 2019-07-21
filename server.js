65a353d7585545fde9636953abf8c72f2ac2b227
Slava <ucheba00@gmail.com>
2019-07-21 00:18:39 -0600

    path fix

--- server.js
+++ server.js
@@ -16,13 +16,23 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 // server static accets in production
+// if (process.env.NODE_ENV === 'production') {
+//   // set static folder
+//   console.log('server running');
+//   app.use(express.static('/client/build'));
+//   console.log(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   );
+// }
 if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('/client/build'));
+  //set static folder
+  console.log('server running');
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.port || 5000;
